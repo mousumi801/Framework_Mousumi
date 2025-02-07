7258d22ebf4c5a2cdbb0545e395127b1175ba40c
Abhilash <Abhilash@DESKTOP-7CMRC7P>
2025-02-07 21:26:29 +0530

    Contacts and org

---
+++ DemoFramework_Mousumi/src/test/java/Contacts/ToCreateContactTest.java
@@ -0,0 +1,10 @@
+package Contacts;
+
+public class ToCreateContactTest {
+
+	public static void main(String[] args) {
+		System.out.println("contact created");
+
+	}
+
+}
---
+++ DemoFramework_Mousumi/src/test/java/Organization/ToCreateOrganizationTest.java
@@ -0,0 +1,10 @@
+package Organization;
+
+public class ToCreateOrganizationTest {
+
+	public static void main(String[] args) {
+		System.out.println("organization created");
+
+	}
+
+}
